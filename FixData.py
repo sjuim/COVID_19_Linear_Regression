d90d96bf9be5b8f4d63514900f64da3f75dda8bc
Shreya Majumdar <59295970+sjuim@users.noreply.github.com>
2020-07-13 18:38:25 -0400

    Commenting what the issue is

--- FixData.py
+++ FixData.py
@@ -11,12 +11,15 @@ import pandas as pd
 #Read the csv file
 dataFrame = pd.read_csv('sampleData.csv', index_col = 'Name')
 
+
+
+#this section isn't working, it's meant to drop all rows that have 'F' in the gender column
 #How to delete rows from the csv file
 for row in dataFrame.itertuples():
     print(row.index)  #each variable row is actually a series I think
-    #print(dataFrame.at[row, 'Gender'])
+    print(dataFrame.at[row, 'Gender'])
         #axis = 0 means delete rows, 1 means columns, #inplace =  false means do not edit the orginal data set
-        #dataFrame.drop(row)
+        dataFrame.drop(row)
 
 
 #Add a new column
