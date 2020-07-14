ac32095f2bfe7a43be290fa4c05e9a0b6ec47ed2
AJ <anthony.james.cavallaro@gmail.com>
2020-07-14 10:41:47 -0400

    quick tutorial added in 'Scoping'

--- FixData.py
+++ FixData.py
@@ -19,18 +19,16 @@ for row in dataFrame.itertuples():
     print(row.index)  #each variable row is actually a series I think
     print(dataFrame.at[row, 'Gender'])
         #axis = 0 means delete rows, 1 means columns, #inplace =  false means do not edit the orginal data set
-        dataFrame.drop(row)
+    dataFrame.drop(row)
 
 
 #Add a new column
 dataFrame['Days After'] = 0
-    
+
 day = 0
 #How to change the value of the new column
 for row in dataFrame.itertuples():
     dataFrame.loc[day:day+1, 'Days After'] = day
     day+=1
 print(dataFrame)
-    
-    
-    
+
